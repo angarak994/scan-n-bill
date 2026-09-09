a74c8387adb5f49a4e93b563e190574fa99ad8a1
phantom-0994 <angarakntate994@gmail.com>
2026-09-09 17:51:56 +0530

    Fix: Remove unsupported eslint config from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // @ts-ignore
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
